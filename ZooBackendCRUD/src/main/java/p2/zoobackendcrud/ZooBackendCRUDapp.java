262c4e52617b00e02f07919ab45544a0bb326b5e
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2022-12-31 18:07:42 -0300

    Creo proyecto de Java en Netbeans. Applicación con springboot para hacer un CRUD de un zoologico. Agregue todas las dependencias.

---
+++ ZooBackendCRUD/src/main/java/p2/zoobackendcrud/ZooBackendCRUDapp.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2.zoobackendcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Agustín Pacheco
+ */
+@SpringBootApplication
+public class ZooBackendCRUDapp {
+    public static void main(String[] args) {
+        SpringApplication.run(ZooBackendCRUDapp.class, args);
+    }
+}
