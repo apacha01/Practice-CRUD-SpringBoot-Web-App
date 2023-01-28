996cc7f5ee95b78973e6ec0467b10a9b9d4ebdcd
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-01-28 19:02:38 -0300

    Creo el controlador de las zonas. Hago el método de Create del CRUD para zonas.

---
+++ ZooBackendCRUD/src/main/java/p2/zoobackendcrud/controllers/ZoneController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2.zoobackendcrud.controllers;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import p2.zoobackendcrud.repositories.ZoneRepository;
+import p2.zoobackendcrud.entities.Zone;
+
+/**
+ *
+ * @author Agustín Pacheco
+ */
+@RestController
+@RequestMapping("/zona")
+public class ZoneController {
+    
+    @Autowired
+    private ZoneRepository znRepo;
+    
+    @PostMapping("/crear")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Zone createZone(@RequestBody Zone z) {
+        if (z == null)
+            return null;
+        return znRepo.save(z);
+    }
+}
