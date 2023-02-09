9fca2b17ee26f8202646e1cf5504cd0d5718de59
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-02-09 18:03:19 -0300

    Creo la clase main y el iniciaizador de servlets. La app corre con el index.html (un hola mundo).

---
+++ ZooFrontendCRUD/src/main/java/p2/zoofrontendcrud/ZooFrontendApp.java
@@ -0,0 +1,25 @@
+package p2.zoofrontendcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Agustín Pacheco
+ */
+@SpringBootApplication
+public class ZooFrontendApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ZooFrontendApp.class, args);
+    }
+    
+}
