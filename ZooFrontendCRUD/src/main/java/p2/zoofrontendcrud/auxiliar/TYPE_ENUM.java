69673a5ba39d9e39b23fcb7c30d15bdd948302b8
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-02-15 11:17:09 -0300

    Creo funcion para obtener el tipo de acuerdo a un string.

--- ZooFrontendCRUD/src/main/java/p2/zoofrontendcrud/auxiliar/TYPE_ENUM.java
+++ ZooFrontendCRUD/src/main/java/p2/zoofrontendcrud/auxiliar/TYPE_ENUM.java
@@ -9,7 +9,26 @@ package p2.zoofrontendcrud.auxiliar;
  * @author Agustín Pacheco
  */
 public enum TYPE_ENUM {
-    ADMIN,
-    KEEPER,
-    GUIDE;
+    ADMIN ("Administrador"),
+    KEEPER ("Cuidador"),
+    GUIDE ("Guia");
+    
+    private final String name;
+    
+    TYPE_ENUM(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public static TYPE_ENUM getTypeFromSpanishString(String s){
+        switch (s) {
+            case "Administrador":   return TYPE_ENUM.ADMIN;
+            case "Cuidador":        return TYPE_ENUM.KEEPER;
+            case "Guia":            return TYPE_ENUM.GUIDE;
+        }
+        return null;
+    }
 }
