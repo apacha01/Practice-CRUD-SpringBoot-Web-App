8633bce6145d575cbbfa308b42c9f08dff8f0f65
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-02-15 11:16:01 -0300

    Agrego constantes para todas las operaciones CRUD de empleados.

--- ZooFrontendCRUD/src/main/java/p2/zoofrontendcrud/auxiliar/Constants.java
+++ ZooFrontendCRUD/src/main/java/p2/zoofrontendcrud/auxiliar/Constants.java
@@ -9,12 +9,14 @@ package p2.zoofrontendcrud.auxiliar;
  * @author Agustín Pacheco
  */
 public class Constants {
-    //Backend Requests Paths for p2.zoobackendcrud.controllers.EmployeeController
     public final static String PREFIX_REQUEST_URL = "http://localhost:8080/";
+    
+    //Backend Requests Paths for p2.zoobackendcrud.controllers.EmployeeController
     public final static String EMPLOYEE_REQUEST_URL = "empleado/";
     public final static String CREATE_EMPLOYEE_REQUEST_URL = "crear";
     public final static String GET_EMPLOYEE_BY_USERNAME_REQUEST_URL = "obtenerpornombredeusuario/";
     public final static String GET_ALL_EMPLOYEES_REQUEST_URL = "obtenertodos";
     public final static String GET_EMPLOYEE_BY_ID_REQUEST_URL = "obtenerporid/";
     public final static String DELETE_EMPLOYEE_BY_ID_REQUEST_URL = "borrar/";
+    public final static String UPDATE_EMPLOYEE_BY_ID_REQUEST_URL = "modificarporid/";
 }
